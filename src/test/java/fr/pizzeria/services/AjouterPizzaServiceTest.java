b73deba990fb5fa2b6e23e514defd12f1d6f9ff4
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-04-24 17:21:38 +0200

    Clean project, Ajouts tests unitaires pour ExecuteUc

--- src/test/java/fr/pizzeria/services/AjouterPizzaServiceTest.java
+++ src/test/java/fr/pizzeria/services/AjouterPizzaServiceTest.java
@@ -1,11 +1,34 @@
 package fr.pizzeria.services;
 
-import static org.junit.Assert.*;
-
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+
+import fr.pizzeria.dao.PizzaMemDao;
+
+import static org.junit.Assert.*;
+import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.*;
+
+import java.util.Scanner;
 
 public class AjouterPizzaServiceTest {
 	
+	@Rule
+	public TextFromStandardInputStream systemInMock = emptyStandardInputStream();
+	
+	@Test
+	public void testExecuteUC()
+	{
+		PizzaMemDao p = new PizzaMemDao();
+		AjouterPizzaService aps = new AjouterPizzaService();
+		
+		systemInMock.provideLines("TES", "Test", "11");
+		Scanner sc = new Scanner(System.in);
+		
+		aps.executeUC(sc, p);
+		
+		assertEquals("TES", p.findAllPizzas().get(8).getCode());
+		assertEquals("Test", p.findAllPizzas().get(8).getLibelle());
+		assertTrue(p.findAllPizzas().get(8).getPrix() == 11);
+	}
 }
