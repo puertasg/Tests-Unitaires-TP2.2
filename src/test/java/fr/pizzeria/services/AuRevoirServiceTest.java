bad335a3af74872f67400043df55ce4d61b6030c
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-04-24 17:49:29 +0200

    Ajout tests unitaires AuRevoirServiceTest

--- src/test/java/fr/pizzeria/services/AuRevoirServiceTest.java
+++ src/test/java/fr/pizzeria/services/AuRevoirServiceTest.java
@@ -1,12 +1,31 @@
 package fr.pizzeria.services;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Scanner;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+
+import fr.pizzeria.dao.PizzaMemDao;
 
 public class AuRevoirServiceTest {
+	
+	@Rule
+	public final SystemOutRule systemOutRule = new SystemOutRule();
 	
 	@Test
 	public void testExecuteUC()
 	{
+		AuRevoirService ars = new AuRevoirService();
+		PizzaMemDao p = new PizzaMemDao();
+		Scanner sc = new Scanner(System.in);
 		
+		systemOutRule.enableLog();
+		
+		ars.executeUC(sc, p);
+		
+		assertEquals("Au revoir\n", systemOutRule.getLogWithNormalizedLineSeparator());
 	}
 }
